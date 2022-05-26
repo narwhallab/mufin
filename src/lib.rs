05054fb90bc56b1a27737309eefb633ee6a9dd67
octomarine <dolgore2410@gmail.com>
2022-05-27 01:37:06 +0900

    v0.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 use std::error::Error;
+use std::str::FromStr;
 use std::time::Duration;
 use std::thread;
-use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter, CharPropFlags, WriteType};
-use btleplug::platform::Manager;
+use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter, CharPropFlags, WriteType, BDAddr};
+use btleplug::platform::{Manager, PeripheralId};
 use napi_derive::napi;
+use log::trace;
+use simplelog::{TermLogger, Config};
 
 #[napi]
-pub async fn bluetooth() {
-    internal().await.expect("Error");
+pub fn init_logger() {
+    TermLogger::init(log::LevelFilter::Trace, Config::default(), simplelog::TerminalMode::Stdout, simplelog::ColorChoice::Auto).unwrap();
 }
 
-async fn internal() -> Result<(), Box<dyn Error>> {
+#[napi]
+pub async fn bluetooth(address: String, message: String) {
+    write_peripheral(&address, &message.as_bytes()).await.expect("Error");
+}
+
+pub async fn get_peripheral(address: &str) -> Result<impl Peripheral, Box<dyn Error>> {
     let manager = Manager::new().await?;
 
     let central = manager
@@ -19,59 +27,56 @@ async fn internal() -> Result<(), Box<dyn Error>> {
         .expect("Unable to fetch adapter list.")
         .into_iter()
         .nth(0)
-        .expect("No adapters are available now...");
-
-    println!("Starting scan on {}...", central.adapter_info().await?);
+        .expect("No adapters are available now...");    // Fetch first adapter
 
     central
         .start_scan(ScanFilter::default())
         .await
         .expect("Can't scan BLE adapter for connected devices...");
 
-    thread::sleep(Duration::from_secs(10)); // Wait until the scan is done
+    trace!("Starting scan on {}...", central.adapter_info().await?);
+        
+    thread::sleep(Duration::from_secs(5)); // Wait until the scan is done
 
-    let peripherals = central.peripherals().await?;
-    if peripherals.is_empty() {
-        eprintln!("->>> BLE peripheral devices were not found, sorry. Exiting...");
-    } else {
-        for peripheral in peripherals.iter() {
-            let properties = peripheral.properties().await?;
-            let is_connected = peripheral.is_connected().await?;
-            let local_name = properties
-                .unwrap()
-                .local_name
-                .unwrap_or(String::from("(peripheral name unknown)"));
-            if !local_name.contains("HMSoft") { // TODO This part
-                continue;
-            }
-            println!("Peripheral {:?} is connected: {:?}", local_name, is_connected);
-            if !is_connected {
-                println!("Connecting to peripheral {:?}...", &local_name);
-                if let Err(err) = peripheral.connect().await {
-                    eprintln!("Error connecting to peripheral, skipping: {}", err);
-                    continue;
-                }
-            }
-            let is_connected = peripheral.is_connected().await?;
-            println!("Now connected ({:?}) to peripheral {:?}...", is_connected, &local_name);
-            peripheral.discover_services().await?;
-            println!("Discover peripheral {:?} services...", &local_name);
-            for service in peripheral.services() {
-                println!("Service UUID {}, primary: {}", service.uuid, service.primary);
-                for characteristic in service.characteristics {
-                    if characteristic.properties.contains(CharPropFlags::WRITE_WITHOUT_RESPONSE) {
-                        peripheral.write(&characteristic, &"on".as_bytes(), WriteType::WithoutResponse).await?;
-                    }
-                }
-            }
-            if is_connected {
-                println!("Disconnecting from peripheral {:?}...", &local_name);
-                peripheral
-                    .disconnect()
-                    .await
-                    .expect("Error disconnecting from BLE peripheral");
+    let peripheral = central.peripheral(&PeripheralId::from(BDAddr::from_str(address).unwrap())).await?;
+
+    Ok(peripheral)
+}
+
+pub async fn write_peripheral(address: &str, bytes: &[u8]) -> Result<(), Box<dyn Error>> {
+    let peripheral = get_peripheral(address).await?;
+    
+    let properties = peripheral.properties().await?;
+    let is_connected = peripheral.is_connected().await?;
+    let local_name = properties
+        .unwrap()
+        .local_name
+        .unwrap_or(String::from("Unknown"));
+
+    if !is_connected {
+        trace!("Connecting to peripheral {}...", &local_name);
+        peripheral.connect().await?;
+    }
+
+    let message = if is_connected { "succeeded" } else { "failed" };
+
+    trace!("Connecting to peripherial: {} has {}", &local_name, message);
+
+    peripheral.discover_services().await?;
+    for service in peripheral.services() {
+        for characteristic in service.characteristics {
+            if characteristic.properties.contains(CharPropFlags::WRITE_WITHOUT_RESPONSE) {
+                peripheral.write(&characteristic, bytes, WriteType::WithoutResponse).await?;
             }
         }
+    }
+
+    if is_connected {
+        trace!("Disconnecting from peripheral {:?}...", &local_name);
+        peripheral
+            .disconnect()
+            .await
+            .expect("Error disconnecting from BLE peripheral");
     }
     Ok(())
 }
